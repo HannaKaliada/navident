d154196e7ac1ff99bc693778850782238fe0e2dc
Hanna Kaliada <hkaliada@lms365.com>
2023-12-18 00:01:18 +0300

    feat: update price and dependencies

--- src/js_modules/switchActiveService.js
+++ src/js_modules/switchActiveService.js
@@ -3,11 +3,13 @@ import showPrice from "./showPrice";
 export default function switchActiveService() {
     const servicesBlock = document.querySelector('.services__list');
     const services = servicesBlock.querySelectorAll('.service');
-    document.querySelector('.services__list').addEventListener('click', (event) => {
-        services.forEach((service) => service.classList.remove('active'));
-        const targetService = event.target.closest('.service');
-        targetService.classList.add('active');
-        const tableNumber = targetService.dataset.table;
-        showPrice(tableNumber);
+    servicesBlock.addEventListener('click', (event) => {
+        if (!event.target.closest('.service').classList.contains('active')) {
+            services.forEach((service) => service.classList.remove('active'));
+            const targetService = event.target.closest('.service');
+            targetService.classList.add('active');
+            const tableNumber = targetService.dataset.table;
+            showPrice(tableNumber);
+        }
     })
 }
